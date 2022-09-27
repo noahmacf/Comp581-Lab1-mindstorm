4a468df809f14c90e19a03f0194b30255e395da7
Noah MacFarlane <noahmacf@gmail.com>
2022-09-27 12:52:33 -0400

    deleted pseudocode

--- main.py
+++ main.py
@@ -11,48 +11,6 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-#pseudocode
-
-#while(program is running)
-#ev3 = EV3brick()
-#start_distance = us_sensor.distance()
-#target_distance = us_sensor.distance() - 1200
-
-#if(button is pressed)
-#while(us_sensor.distance() < target_distance)
-#motor1.run(180)
-#motor2.run(180)
-
-#if(us_sensor.distance() == target_distance)
-#motor1.brake()
-#motor2.brake()
-#ev3.speaker.beep()
-
-#if(button is pressed again)
-#while(us_sensor.distance() > 500)
-#motor1.run(180)
-#motor2.run(180)
-
-#if(us_sensor.distance() == 500)
-#motor1.brake()
-#motor2.brake()
-#ev3.speaker.beep()
-
-#if(button press again)
-#		motor1.run(180)
-#motor2.run(180)
-#	if(touch_sensor.pressed())
-#		motor1.brake()
-#		motor2.brake()
-#		while(us_sensor.distance() < 500)
-#			motor1.run(-180)
-#`			motor2.run(-180)
-#				if(us_sensor.distance() == 500)
-#                   motor1.brake()
-#                   motor2.brake()
-
-
-
 # Create your objects here.
 ev3 = EV3Brick()
 left_motor = Motor(Port.B)
